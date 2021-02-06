c785534cea3c73a0160aa35ca0a3a3c13d9382d9
Miras Rambaev <shadowbroker2176@gmail.com>
2021-02-06 21:42:51 +0300

    Start post creation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './interfaces';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello my friends!';
-
-  inputText = '';
-
-  changeText(e) {
-    this.inputText = e.target.value;
-  }
+  posts: Post[] = [
+    {
+      title: 'Want everyone to be happy!',
+      text: 'lorem10',
+      id: 1
+    },
+    {
+      title: 'Want to live forever',
+      text: 'lorem10',
+      id: 2
+    }
+  ];
 }
